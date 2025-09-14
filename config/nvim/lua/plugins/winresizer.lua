60e0c7bb1bff43a5a7161a76119558d8949341df
chukyapin <overheadp1pp1@gmail.com>
2025-09-15 00:15:40 +0900

    clean up

--- config/nvim/lua/plugins/winresizer.lua
+++ config/nvim/lua/plugins/winresizer.lua
@@ -1,6 +1,9 @@
 return {
   "simeji/winresizer",
-  keys = {
-    { "<C-e>", "<Plug>WinResizerStartResize" },
-  },
+  keys = function()
+    local opts = { noremap = true, silent = true }
+    return {
+      { "<C-e>", "<Plug>WinResizerStartResize", opts },
+    }
+  end,
 }
