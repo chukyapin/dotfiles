09ba0aba05fe0db5a97a442903a498206b66c818
chukyapin <overheadp1pp1@gmail.com>
2025-09-15 00:14:40 +0900

    clean up

--- config/nvim/lua/plugins/LuaSnip.lua
+++ config/nvim/lua/plugins/LuaSnip.lua
@@ -3,8 +3,12 @@ return {
   lazy = true,
   run = "make install_jsregexp",
   config = function()
-    require("luasnip").setup({})
+    require("luasnip").setup({ enable_autosnippets = true })
     require("luasnip.loaders.from_snipmate").lazy_load()
-    require("luasnip.loaders.from_lua").lazy_load()
+    require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/snippets/" })
+    local ls = require("luasnip")
+    vim.keymap.set("i", "<C-e>", function()
+      ls.expand_or_jump(1)
+    end, { silent = true })
   end,
 }
