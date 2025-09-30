232797993de8af88428d91bab499224a1dd35ddd
chukyapin <overheadp1pp1@gmail.com>
2025-09-30 19:32:23 +0900

    init

--- config/nvim/lua/plugins/aerial_nvim.lua
+++ config/nvim/lua/plugins/aerial_nvim.lua
@@ -1,20 +1,32 @@
 return {
   "stevearc/aerial.nvim",
   opts = {
+    backends = { "lsp", "treesitter", "markdown", "asciidoc", "man" },
     layout = {
-      default_direction = "float",
       min_width = 50,
     },
     keymaps = {
       ["<ESC>"] = "actions.close",
       ["o"] = "actions.scroll",
     },
+    highlight_on_hover = true,
+    close_on_select = true,
+
+    -- ネストは1階層まで
+    on_first_symbols = function(bufnr)
+      require("aerial").tree_set_collapse_level(bufnr, 1)
+    end,
+    -- tree_set_collapse_level が連動してしまうので必要
+    link_tree_to_folds = false,
+
+    -- 表示は上の方に
+    post_jump_cmd = "normal! zt",
   },
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
   },
   keys = {
-    { "<A-j>o", ":AerialOpen<CR>", silent = true },
+    { "<C-j>o", ":AerialOpen<CR>", silent = true },
   },
   init = function()
     vim.api.nvim_create_autocmd("ColorScheme", {
