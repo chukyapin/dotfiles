9b63a7da4de754e795a6874f061336e107ddcda7
chukyapin <overheadp1pp1@gmail.com>
2025-10-13 16:07:59 +0900

    init

---
+++ config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,47 @@
+-- return {
+--   "zbirenbaum/copilot.lua",
+--   cmd = "Copilot",
+--   event = "InsertEnter",
+--   opts = {
+--     suggestion = {
+--       auto_trigger = true,
+--       keymap = {
+--         accept = "<D-k>",
+--       },
+--     },
+--     filetypes = {
+--       javascript = true,
+--       typescript = true,
+--       lua = true,
+--       go = true,
+--       vue = true,
+--       python = true,
+--       rust = true,
+--       ["*"] = false,
+--     },
+--   },
+-- }
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      server_opts_overrides = {
+        trace = "verbose",
+        cmd = {
+          vim.fn.expand("~/.local/share/mise/shims/copilot-language-server"),
+          "--stdio",
+        },
+        settings = {
+          advanced = {
+            listCount = 10,
+            inlineSuggestCount = 3,
+          },
+        },
+      },
+    })
+  end,
+}
