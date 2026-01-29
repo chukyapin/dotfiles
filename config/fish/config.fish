0c796ef5dd82e86d2bdb3a4b40e2466324671399
chukyapin <overheadp1pp1@gmail.com>
2026-01-29 19:18:30 +0900

    init

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -20,7 +20,6 @@ fish_add_path /bin
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.luarocks/bin
-# fish_add_path ~/Library/Python/3.{8,9}/bin
 fish_add_path ~/Library/Python/3.14/bin
 fish_add_path /usr/local/opt/sqlite/bin
 fish_add_path /usr/local/sbin
@@ -29,8 +28,8 @@ fish_add_path ~/.local/bin/pnpm-bins
 fish_add_path ~/.local/share/bob-nvim/bin
 fish_add_path ~/.local/share/bob-nvim/nvim-linux64/bin
 fish_add_path /var/lib/flatpak/exports/bin/
-fish_add_path ~/.local/share/mise/shims
 fish_add_path ~/.dotnet/tools
+fish_add_path ~/.local/share/mise/shims
 
 set -gx DENO_INSTALL "~/.deno"
 fish_add_path ~/.deno/bin
