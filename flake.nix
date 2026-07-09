61e271a40921a1eb8a31899ffc94704b295dd774
katayama <chukyapin@katayamanoMacBook-Pro.local>
2026-07-09 17:54:21 +0900

    generate Home Manager config file

--- flake.nix
+++ flake.nix
@@ -1,56 +1,31 @@
 {
-  description = "My dotfiles";
+  description = "Home Manager configuration of chukyapin";
 
   inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nix-darwin = {
-      url = "github:nix-darwin/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      home-manager,
-      nix-darwin,
-      nix-homebrew,
-      treefmt-nix,
-      ...
-    }:
+    { nixpkgs, home-manager, ... }:
     let
-      username = "chukyapin";
-      darwinHomedir = "/Users/${username}";
-      system = "aarch64-darwin"; # Intel なら "x86_64-darwin"
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-      darwinConfigurations."katayamanoMacBook-Pro" =
-        nix-darwin.lib.darwinSystem {
-          inherit system;
+      homeConfigurations."chukyapin" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
 
-          specialArgs = {
-            inherit self nix-homebrew usename darwinHomedir;
-          };
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
 
-          modules = [
-            ./nix/modules/darwin/configuration.nix
-            home-manager.darwinModules.home-manager
-            nix-homebrew.darwinModules.nix-homebrew
-          ];
-        };
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
     };
 }
