22505189c659c24f03df02be3cb71950b88b042a
katayama <chukyapin@katayamanoMacBook-Pro.local>
2026-07-09 18:17:12 +0900

    Pin nixpkgs to working commit for nix-darwin manual

--- flake.nix
+++ flake.nix
@@ -3,15 +3,24 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/8809585e6937d0b07fc066792c8c9abf9c3fe5c4";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-darwin = {
+       url = "github:nix-darwin/nix-darwin";
+       inputs.nixpkgs.follows = "nixpkgs";
+     };
   };
 
   outputs =
-    { nixpkgs, home-manager, ... }:
+    { 
+      nixpkgs,
+      home-manager,
+      nix-darwin,
+      ...
+      }:
     let
       system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -27,5 +36,8 @@
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
       };
+      darwinConfigurations."katayamanoMacBook-Pro" = nix-darwin.lib.darwinSystem {
+         modules = [ ./nix-darwin/configuration.nix ];
+       };
     };
 }
