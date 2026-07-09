ec4ee23288307e3af66d4ae96f7cb97f8bcca50e
katayama <chukyapin@katayamanoMacBook-Pro.local>
2026-07-09 19:24:15 +0900

    Add home-manager home.nix and nix-darwin configs

---
+++ home-manager/home.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "chukyapin";
+  home.homeDirectory = "/Users/chukyapin";
+  home.stateVersion = "26.05"; # Please read the comment before changing.
+  home.packages = [
+  ];
+  home.file = {
+  };
+
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
