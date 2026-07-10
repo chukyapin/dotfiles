bcaf6dd31a8ae08d4ab8f74f83542f3dd1d92a4c
katayama <chukyapin@katayamanoMacBook-Pro.local>
2026-07-10 14:04:11 +0900

    Minimal Home Manager flake setup

--- flake.nix
+++ flake.nix
@@ -1,69 +1,28 @@
+
 {
-  description = "Home Manager configuration of chukyapin";
+  description = "Home Manager configuration of chukyapin (minimal)";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
-    nix-darwin = {
-      url = "github:nix-darwin/nix-darwin/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+    # macOS なので nixpkgs-* ブランチを使う
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # もし使うなら treefmt-nix や brew-nix などもここに
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    brew-nix = {
-      url = "github:BatteredBunny/brew-nix";
-      inputs = {
-        brew-api.follows = "brew-api";
-        nix-darwin.follows = "nix-darwin";
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-    brew-api = {
-      url = "github:BatteredBunny/brew-api";
-      flake = false;
-    };
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      home-manager,
-      nix-darwin,
-      nix-homebrew,
-      ...
-    }:
-    let
-      system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      # Home Manager 単体で使うなら
-      homeConfigurations."chukyapin" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        # いま home.nix はルート直下にある
-        modules = [ ./home.nix ];
-      };
-
-      darwinConfigurations."katayamanoMacBook-Pro" =
-        nix-darwin.lib.darwinSystem {
-          specialArgs = {
-            inherit self nix-homebrew;
-          };
-          modules = [
-            # いま configuration.nix は ./nix/nix-darwin 配下
-            ./nix/nix-darwin/configuration.nix
-            home-manager.darwinModules.home-manager
-            nix-homebrew.darwinModules.nix-homebrew
-          ];
-        };
+  outputs = { self, nixpkgs, home-manager, ... }:
+  let
+    system = "aarch64-darwin";  # Intel Mac なら "x86_64-darwin"
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    # Home Manager Standalone 用
+    homeConfigurations."chukyapin" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [
+        ./home-manager/home.nix
+      ];
     };
+  };
 }
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,18 +1,17 @@
+
+# home-manager/home.nix
 { config, pkgs, ... }:
 
 {
   home.username = "chukyapin";
   home.homeDirectory = "/Users/chukyapin";
-  home.stateVersion = "26.05"; # Please read the comment before changing.
-  home.packages = [
+  home.stateVersion = "26.05";  # 本に合わせる
+
+  # とりあえず最小限のパッケージだけ
+  home.packages = with pkgs; [
+    git
   ];
-  home.file = {
-  };
 
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
-
-  # Let Home Manager install and manage itself.
+  # Home Manager 自身の管理は有効化しておく
   programs.home-manager.enable = true;
 }
