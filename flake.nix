542a3e19a1b8415ba7582e565a8783db8baff34d
katayama <chukyapin@katayamanoMacBook-Pro.local>
2026-07-10 11:50:07 +0900

    Fix flake paths for nix-darwin and home-manager

--- flake.nix
+++ flake.nix
@@ -2,42 +2,68 @@
   description = "Home Manager configuration of chukyapin";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-darwin = {
-       url = "github:nix-darwin/nix-darwin/master";
-       inputs.nixpkgs.follows = "nixpkgs";
-     };
+
+    # もし使うなら treefmt-nix や brew-nix などもここに
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    brew-nix = {
+      url = "github:BatteredBunny/brew-nix";
+      inputs = {
+        brew-api.follows = "brew-api";
+        nix-darwin.follows = "nix-darwin";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    brew-api = {
+      url = "github:BatteredBunny/brew-api";
+      flake = false;
+    };
   };
 
   outputs =
-    { 
+    {
+      self,
       nixpkgs,
       home-manager,
       nix-darwin,
+      nix-homebrew,
       ...
-      }:
+    }:
     let
       system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
+      # Home Manager 単体で使うなら
       homeConfigurations."chukyapin" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
+        # いま home.nix はルート直下にある
         modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
       };
-      darwinConfigurations."katayamanoMacBook-Pro" = nix-darwin.lib.darwinSystem {
-         modules = [ ./nix-darwin/configuration.nix ];
-       };
+
+      darwinConfigurations."katayamanoMacBook-Pro" =
+        nix-darwin.lib.darwinSystem {
+          specialArgs = {
+            inherit self nix-homebrew;
+          };
+          modules = [
+            # いま configuration.nix は ./nix/nix-darwin 配下
+            ./nix/nix-darwin/configuration.nix
+            home-manager.darwinModules.home-manager
+            nix-homebrew.darwinModules.nix-homebrew
+          ];
+        };
     };
 }
