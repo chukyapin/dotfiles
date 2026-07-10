ad0eb8c04b75dbe815e6713446a202986186bfbe
katayama <chukyapin@katayamanoMacBook-Pro.local>
2026-07-10 17:54:03 +0900

    generate Home Manager config file

--- flake.nix
+++ flake.nix
@@ -1,28 +1,31 @@
-
 {
-  description = "Home Manager configuration of chukyapin (minimal)";
+  description = "Home Manager configuration of chukyapin";
 
   inputs = {
-    # macOS なので nixpkgs-* ブランチを使う
+    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
-  let
-    system = "aarch64-darwin";  # Intel Mac なら "x86_64-darwin"
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    # Home Manager Standalone 用
-    homeConfigurations."chukyapin" = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      modules = [
-        ./home-manager/home.nix
-      ];
+  outputs =
+    { nixpkgs, home-manager, ... }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations."chukyapin" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
     };
-  };
 }
