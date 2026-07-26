1c400e7d6ca29e3dd610f767df47001f7a76fbcf
chukyapin <overheadp1pp1@gmail.com>
2026-07-26 13:04:17 +0900

    keymaps

--- nvim/lua/plugins/dial_nvim.lua
+++ nvim/lua/plugins/dial_nvim.lua
@@ -1,21 +1,72 @@
 return {
   -- Better increase/descrease
   "monaqa/dial.nvim",
-    -- stylua: ignore
-    keys = {
-      {
-        "<C-a>",
-        function() return require("dial.map").inc_normal() end,
-        expr = true,
-        desc = "Increment",
-      },
-      {
-        "<C-x>",
-        function() return require("dial.map").dec_normal() end,
-        expr = true,
-        desc = "Decrement",
-      },
+  keys = {
+    {
+      "<C-a>",
+      function()
+        require("dial.map").manipulate("increment", "normal")
+      end,
+      mode = "n",
+      desc = "dial increment",
     },
+    {
+      "<C-x>",
+      function()
+        require("dial.map").manipulate("decrement", "normal")
+      end,
+      mode = "n",
+      desc = "dial decrement",
+    },
+    {
+      "g<C-a>",
+      function()
+        require("dial.map").manipulate("increment", "gnormal")
+      end,
+      mode = "n",
+      desc = "dial increment",
+    },
+    {
+      "g<C-x>",
+      function()
+        require("dial.map").manipulate("decrement", "gnormal")
+      end,
+      mode = "n",
+      desc = "dial decrement",
+    },
+    {
+      "<C-a>",
+      function()
+        require("dial.map").manipulate("increment", "visual")
+      end,
+      mode = "v",
+      desc = "dial increment",
+    },
+    {
+      "<C-x>",
+      function()
+        require("dial.map").manipulate("decrement", "visual")
+      end,
+      mode = "v",
+      desc = "dial decrement",
+    },
+    {
+      "g<C-a>",
+      function()
+        require("dial.map").manipulate("increment", "gvisual")
+      end,
+      mode = "v",
+      desc = "dial increment",
+    },
+    {
+      "g<C-x>",
+      function()
+        require("dial.map").manipulate("decrement", "gvisual")
+      end,
+      mode = "v",
+      desc = "dial decrement",
+    },
+  },
   config = function()
     local augend = require("dial.augend")
     require("dial.config").augends:register_group({
@@ -25,7 +76,6 @@ return {
         augend.date.alias["%Y/%m/%d"],
         augend.date.alias["%Y-%m-%d"],
         augend.date.alias["%m/%d"],
-        -- augend.date.alias["%m-%d"],
         augend.constant.alias.bool,
         augend.constant.alias.ja_weekday,
         augend.constant.alias.ja_weekday_full,
@@ -34,10 +84,10 @@ return {
         augend.constant.new({ elements = { "==", "!=" }, word = false, cyclic = true }),
         augend.constant.new({ elements = { "and", "or" } }),
         augend.constant.new({ elements = { "let", "const" } }),
-        -- augend.case.new({
-        --   types = { "camelCase", "snake_case", "PascalCase", "SCREAMING_SNAKE_CASE" },
-        --   cyclic = true,
-        -- }),
+        augend.case.new({
+          types = { "camelCase", "snake_case", "PascalCase", "SCREAMING_SNAKE_CASE" },
+          cyclic = true,
+        }),
       },
     })
   end,
