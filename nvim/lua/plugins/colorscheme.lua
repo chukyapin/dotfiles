433e18e862416b98e14a75383e1fca67c2142bdd
chukyapin <overheadp1pp1@gmail.com>
2026-07-29 18:26:14 +0900

    cleanup

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -13,26 +13,3 @@ return {
     vim.cmd("colorscheme gruvbox")
   end,
 }
-
--- return {
---   -- "craftzdog/solarized-osaka.nvim",
---   -- "folke/tokyonight.nvim",
---   "https://github.com/ellisonleao/gruvbox.nvim",
---   cond = true,
---   priority = 1000,
---   config = function()
---     local transparent = true -- set to true if you would like to enable transparency
---     -- require("solarized-osaka").setup({
---     -- require("tokyonight").setup({
---     require("gruvbox").setup({
---       transparent = transparent,
---       styles = {
---         sidebars = transparent and "transparent" or "dark",
---         floats = transparent and "transparent" or "dark",
---       },
---     })
---     -- vim.cmd("colorscheme solarized-osaka")
---     -- vim.cmd("colorscheme tokyonight")
---     vim.cmd("colorscheme gruvbox")
---   end,
--- }
