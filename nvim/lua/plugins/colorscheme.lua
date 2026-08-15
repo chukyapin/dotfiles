ba7437cbaedcbf7626ade2c392d157d43d5cceb3
chukyapin <overheadp1pp1@gmail.com>
2026-08-15 20:56:37 +0900

    change colorsheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,19 @@
 return {
-  "ellisonleao/gruvbox.nvim",
+  -- "ellisonleao/gruvbox.nvim",
+  "folke/tokyonight.nvim",
   cond = true,
   priority = 1000,
   config = function()
-    require("gruvbox").setup({
-      transparent_mode = true,
+    -- require("gruvbox").setup({
+    require("tokyonight").setup({
+      -- transparent_mode = true,
+      transparent = true,
       styles = {
         sidebars = "transparent",
         floats = "transparent",
       },
     })
-    vim.cmd("colorscheme gruvbox")
+    -- vim.cmd("colorscheme gruvbox")
+    vim.cmd("colorscheme tokyonight")
   end,
 }
