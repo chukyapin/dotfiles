f5358e05a68ef485755c142f4bcd34fd429b9df2
chukyapin <overheadp1pp1@gmail.com>
2026-08-17 20:22:39 +0900

    cleanup

--- nvim/lua/plugins/typst_preview_nvim.lua
+++ nvim/lua/plugins/typst_preview_nvim.lua
@@ -1,5 +1,5 @@
 return {
-  "chomosuke/typst-preview.nvim",
+  "https://github.com/chomosuke/typst-preview.nvim",
   ft = "typst",
   version = "1.*",
   opts = {}, -- lazy.nvim will implicitly calls `setup {}`
